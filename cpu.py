6d83b44cc504c98e732297bfc1144407fbfd3865
bk0704 <bilal.k1@icloud.com>
2025-08-18 22:38:56 -0400

    initialized CPU class

---
+++ cpu.py
@@ -0,0 +1,61 @@
+# Emulate LR35902 CPU state and lifecycle
+
+class CPU:
+    def __init__(self, bus):
+        self.bus = bus # the thing with read8/write8
+
+        # registers
+
+        #8-bit
+        self.A = 0x00 # Accumulation
+        self.F = 0x00 # Flags
+        self.B = 0x00
+        self.C = 0x00
+        self.D = 0x00
+        self.E = 0x00
+        self.H = 0x00
+        self.L = 0x00
+
+        # Special 16-bit
+        self.PC = 0x0000
+        self.SP = 0x0000
+
+        # Control State
+        self.IME = False # Interrupt Master enable: Master switch that decides if the CPU is allowed service interupts
+        self.halted = False # Shows if CPU is in halt mode
+        self.stopped = False # shows if the CPU is in STOP mode (an even deeper sleep, usually triggered manually, sometimes tied to hardware like button presses or the Game Boy Color’s speed switch).
+        self.ei_pending = False # a special flip-flop used for the EI instruction.
+
+        # Timing
+        self.cycles_total = 0
+        self.last_instr_cycles = 0
+
+    def reset(self, post_boot=True):
+        self.A = 0x01
+        self.F = 0xB0
+        self.B = 0x00
+        self.C = 0x13
+        self.D = 0x00
+        self.E = 0xD8
+        self.H = 0x01
+        self.L = 0x4D
+        self.PC = 0x0100
+        self.SP = 0xFFFE
+
+        self.IME = False
+        self.halted = False
+        self.stopped = False
+        self.ei_pending = False
+
+        self.cycles_total = 0
+        self.last_instr_cycles = 0
+
+        self.F &= 0xF0
+
+
+
+
+
+
+
+
